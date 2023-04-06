eceb1ef39e3073d2156cd8eae309e7808cc774a9
Casey1122 <caseyccs1122@gmail.com>
2023-04-06 15:37:58 +0800

    unique order array and quantity count function added

--- components/Order.tsx
+++ components/Order.tsx
@@ -1,21 +1,70 @@
 import styles from "@/styles/Home.module.css";
 import { useCurrentOrderStore } from "@/stores/OrderStore";
+import { useEffect, useState } from "react";
 
 function Order() {
+  /* ==================== VALUE FROM STORE ==================== */
   const currentOrder = useCurrentOrderStore((state) => state.currentOrder);
   const clearCurrentOrder = useCurrentOrderStore(
     (state) => state.clearCurrentOrder
   );
+  const totalAmount = useCurrentOrderStore((state) => state.totalAmount);
+  const setTotalAmount = useCurrentOrderStore((state) => state.setTotalAmount);
+  const uniqueCurrentOrder = useCurrentOrderStore(
+    (state) => state.uniqueCurrentOrder
+  );
+  const setUniqueCurrentOrder = useCurrentOrderStore(
+    (state) => state.setUniqueCurrentOrder
+  );
 
-  const currentOrderElement = currentOrder.map((item, index) => {
+  const currentOrderElement = uniqueCurrentOrder.map((item, index) => {
     return (
-      <div key={index}>
-        <p>{item.name_tc}</p>
-        <p>{item.name_en}</p>
-        <p>{item.price}</p>
+      <div key={index} className={styles.orderItem}>
+        <div className={styles.itemTitle}>
+          <p>{item.product.name_tc}</p>
+          <p>{item.product.name_en}</p>
+        </div>
+        <p>{item.quantity}</p>
+        <p>${item.product.price}</p>
       </div>
     );
   });
+
+  useEffect(() => {
+    calculateTotal();
+    setUniqueCurrentOrder(currentOrder, uniqueCurrentOrder);
+  }, [currentOrder]);
+
+  // console.log("currentOrder", currentOrder);
+  console.log("uniqueCurrentOrder", uniqueCurrentOrder);
+
+  function calculateTotal() {
+    const result = currentOrder
+      .map((item) => item.price)
+      .reduce((accumulator, current) => accumulator + current, 0);
+    setTotalAmount(result);
+  }
+
+  // function itemCount(id: number) {
+  //   // @ts-ignore
+  //   const uniqueItems = [...new Set(currentOrder)];
+  //   const count = uniqueItems.map((item) => [
+  //     item,
+  //     currentOrder.filter((element) => element.id === id).length,
+  //   ]);
+  //   console.log("count", count);
+  //   return count;
+  // }
+
+  // function itemCount2() {
+  //   // @ts-ignore
+  //   const uniqueItemList = [...new Set(currentOrder)];
+  //   const uniqueItemListWithQuantity = uniqueItemList.map((item) => ({
+  //     item,
+  //     quantity: 1,
+  //   }));
+  //   setUniqueItem(uniqueItemListWithQuantity);
+  // }
 
   return (
     <div className={styles.orderSection}>
@@ -26,11 +75,11 @@ function Order() {
         <p>Amount</p>
       </div>
       <div className={styles.itemList}>
-        {currentOrder ? currentOrderElement : <p>No item yet</p>}
+        {currentOrder.length ? currentOrderElement : <p>No item yet</p>}
       </div>
       <div className={styles.itemCost}>
         <p>TOTAL:</p>
-        <p>$0</p>
+        <p>${totalAmount}</p>
       </div>
       <hr />
       <div className={styles.buttonGroup}>
--- stores/OrderStore.tsx
+++ stores/OrderStore.tsx
@@ -5,6 +5,18 @@ export interface CurrentOrderStore {
   currentOrder: Product[];
   addToCurrentOrder: (item: Product) => void;
   clearCurrentOrder: () => void;
+  totalAmount: number;
+  setTotalAmount: (amount: number) => void;
+  uniqueCurrentOrder: UniqueCurrentOrder[];
+  setUniqueCurrentOrder: (
+    currentOrder: Product[],
+    uniqueCurrentOrder: UniqueCurrentOrder[]
+  ) => void;
+}
+
+export interface UniqueCurrentOrder {
+  product: Product;
+  quantity: number;
 }
 
 export const useCurrentOrderStore = create<CurrentOrderStore>((set) => ({
@@ -12,4 +24,33 @@ export const useCurrentOrderStore = create<CurrentOrderStore>((set) => ({
   addToCurrentOrder: (item: Product) =>
     set((state) => ({ ...state, currentOrder: [...state.currentOrder, item] })),
   clearCurrentOrder: () => set({ currentOrder: [] }),
+  totalAmount: 0,
+  setTotalAmount: (amount) => set({ totalAmount: amount }),
+  uniqueCurrentOrder: [],
+  setUniqueCurrentOrder: (currentOrder, uniqueCurrentOrder) =>
+    set({
+      uniqueCurrentOrder: createUniqueCurrentOrder(
+        currentOrder,
+        uniqueCurrentOrder
+      ),
+    }),
 }));
+
+function createUniqueCurrentOrder(
+  currentOrder: Product[],
+  uniqueCurrentOrder: UniqueCurrentOrder[]
+) {
+  const count = {};
+  currentOrder.forEach((item) => {
+    // @ts-ignore
+    count[item.id] = (count[item.id] || 0) + 1;
+  });
+  // @ts-ignore
+  const uniqueItemList: UniqueCurrentOrder = [...new Set(currentOrder)].map(
+    (product) => ({
+      product,
+      quantity: count[product.id],
+    })
+  );
+  return uniqueItemList;
+}
